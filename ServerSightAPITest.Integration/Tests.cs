7e2c85a7b2c15b5d73a162aacb06d242a88e7406
lukasberk <lukasvdberk@gmail.com>
2021-05-14 16:50:51 +0200

    [Added] Integration testing setup

---
+++ ServerSightAPITest.Integration/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace ServerSightAPITest.Integration
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
